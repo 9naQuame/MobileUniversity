fbb0bb756a8ef60c027d5c5b9bfe1ac29969eca1
Nana Ansah Baidoo <stylish17@ymail.com>
2011-07-07 14:13:26 +0000

    added more models

--- myMobileUni/mobile/models.py
+++ myMobileUni/mobile/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Picture(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+    created = models.DateField()
+    updated = models.DateField()
+    def __unicode__(self):
+        return self.name
+
+class Emergency(models.Model):
+    name = models.CharField(max_length=60)
+    number = models.IntegerField()
+    location = models.TextField()
+    def __unicode__(self):
+	return self.name
+
+class Calendar(models.Model):
+    datetime = models.DateTimeField()
+    event = models.TextField()
+    semester = models.IntegerField()
+    def __unicode__(self):
+	return self.event
