eded14b37418baa6aac15563377455784ae00f96
ladyasaph <ladyasaph@gmail.com>
2011-07-08 14:28:41 +0000

    adding the models major, course, timetable

--- myMobileUni/mobile/models.py
+++ myMobileUni/mobile/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+class Course(models.Model):
+	code = models.CharField(max_length = 10)
+	name = models.CharField(max_length = 40)
+	abbreviation = models.CharField(max_length = 10)
 
-# Create your models here.
+class Timetable(models.Model):
+	examVenue = models.CharField(max_length = 10)
+	examTime = models.DateField()
+	lectureVenue = models.CharField(max_length = 10)
+	lectureTime = models.DateField()
+	semester = models.IntegerField()
+	year = models.IntegerField()
+	code = models.ForeignKey(Course)
+	major = models.ForeignKey(Major)
+
+class Major(models.Model):
+	faculty = models.CharField(max_length = 60)
+	department = models.CharField(max_length = 60)
+	majors = models.CharField(max_length = 60)
+
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
