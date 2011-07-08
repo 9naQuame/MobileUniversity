d59a377fd2c9a99984b08ca0e25559d7e1edf308
Nana Kwame <nanakwame123@gmail.com>
2011-07-08 14:50:48 +0000

    event, news and announcements models added

--- myMobileUni/mobile/models.py
+++ myMobileUni/mobile/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+class event(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	venue = models.TextField()
+	time = models.DateTimeField(auto_now_add=True)
+	rate = models.DecimalField(max_digits=10, decimal_places=2)
+	organisers = models.TextField()
+	approval = models.BooleanField()
+	eventtype = models.BooleanField()
+	status = models.CharField(max_length=10)
+	def __unicode__(self):
+		return self.title
 
-# Create your models here.
+class news(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	created = models.DateField(auto_now_add=True)
+	updated = models.DateField(auto_now=True)
+	author = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.title
+
+class announcement(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	created = models.DateField(auto_now_add=True)
+	updated = models.DateField(auto_now=True)
+	def __unicode__(self):
+		return self.title
