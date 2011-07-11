6d62abde4f9ab6e81b49fad57bfce2d44dc6c2b5
ladyasaph <ladyasaph@gmail.com>
2011-07-11 11:00:37 +0000

    changed timetable model to exams, changes to course, adding view for faculty, department, course, exams

--- myMobileUni/mobile/views.py
+++ myMobileUni/mobile/views.py
@@ -1,6 +1,6 @@
 from django.template import Context, loader
 from django.http import HttpResponse, HttpResponseRedirect
-from models import new, event, announcement
+from models import new, event, announcement, Faculty, Department,Course, Exam
 from django import forms
 from django.forms import ModelForm
 from django.views.decorators.csrf import csrf_exempt
@@ -54,3 +54,43 @@ def add_event(request):
 	c = Context({'form':form.as_p()})
 	return HttpResponse(t.render(c))
 
+#Lady-Asaph
+#faculty list
+def faculty_list(request, limit=100):
+	'''faculty_list = Faculty.objects.all()
+	t = loader.get_template('mobile/facultylist.html')
+	c = Context({'faculty':faculty_list})
+	return HttpResponse(t.render(c))'''
+	return HttpResponse('going to give a list')
+
+
+def faculty_department(request, id, limit=100):
+	faculty_list = Faculty.objects.get(pk=id)
+	department_list = Department.objects.all(Faculty__id=id)
+	t = loader.get_template('mobile/departmentlist.html')
+	c = Context({'faculty':faculty_list, 'department':department_list})
+	return HttpResponse(t.render(c))
+
+def course_department(request, id, limit=100):
+	department_list = Department.objects.get(pk=id)
+	course_list = Course.objects.all(Department__id = id)
+	t = loader.get_template('mobile/courselist.html')
+	c = Context({'department':department_list, 'course':course_list})
+	return HttpResponse(t.render(c))
+
+def exam_timetable(request, id, limit=100):
+	#course_list = Course.objects.filter(department__id=id)
+	#ids = [course.id for course in course_list]
+	#exam_list = Exam.objects.filter(course__id__in=ids)
+	# --or--
+	# exam_list = Exam.objects.filter(course__department__id=id) # (not sure if this works with django-nonrel)
+
+	exam_list = Exam.objects.filter(department__id=id)
+	#print exam_list
+
+	'''
+	department = Department.objects.get(pk=id)
+	exam_list = Exam.objects.filter(course in list of courses
+	course_list = Course.objects.all(post_pk = id)
+	timetable_list= TimeTable.objects.all()
+	'''
