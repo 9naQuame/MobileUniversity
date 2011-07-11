873bc48e9dd2ea3aaae87c40b79d7b86e50cbc94
ladyasaph <ladyasaph@gmail.com>
2011-07-11 14:52:21 +0000

    they said i shouldo this

--- myMobileUni/settings.py
+++ myMobileUni/settings.py
@@ -119,7 +119,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'mobile'
+    'mobile',
+    'django_evolution'
 )
 
 # A sample logging configuration. The only tangible logging
